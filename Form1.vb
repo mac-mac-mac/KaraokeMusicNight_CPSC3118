ccef988155f5f051e99f211dc86e63cad0c520eb
Mac Barron <barron_mackenzie@ad1.columbusstate.edu>
2023-04-11 11:16:32 -0400

    Set up button and label layout for main form

--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class KaraokeMusicNight
-
+    Private Sub KaraokeMusicNight_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblSongsHours.Visible = False
+        txtInputNum.Visible = False
+        btnTotalCost.Visible = False
+        btnClear.Visible = False
+        lblTotalCost.Visible = False
+    End Sub
 End Class
